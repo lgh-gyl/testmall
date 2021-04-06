b291d55e68f5b3812f997ec756610fba14743fd2
lghs <lguo_s@163.com>
2021-04-06 19:07:39 +0800

    新增数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,12 +2,14 @@
 <div id="home">
     <nav-bar class="nav-bar"><div slot="center">购物车</div></nav-bar>
     <homewpper :banners='banners'></homewpper>
+    <recommed-view :recommends='recommends'></recommed-view>
  <h2>我是首页</h2>
 </div> 
 </template>
 <script>
   import NavBar from '../../components/navbar/NavBar'
   import Homewpper from '../../views/home/childComps/Homewpper'
+  import RecommedView from '../../views/home/childComps/RecommedView'
 
   import {getHomeMultidata} from '../../network/home'
 
@@ -16,22 +18,24 @@ export default {
       name:'Home',
         components:{
           NavBar,
-          Homewpper     
+          Homewpper,
+          RecommedView
        },
        data() {
            return {
               banners:[],
               recommends:[]
+             
            }
        },
       created() {
         //    1，请求数据
          getHomeMultidata().then(res =>{
-            // console.log(res,2222)
+               console.log(res,2222)
             // this.result = res.data;
              this.banners =res.data.banner.list;
              this.recommends = res.data.recommend.list;
-
+            this.keywords =res.data.keywords.list;
          })
 
       },
---
+++ src/views/home/childComps/RecommedView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="reommend">
+      <div v-for="(item,index) in recommends"  :key="index" class="reommend-tiem">
+          <a :href="item.link">
+              <img :src="item.image" alt="">
+          </a>
+          <div>{{item.title}}</div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {
+    name:'RecommedView',
+    props:{
+        recommends:{
+            type:Array,
+            default(){
+              return []
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .reommend{
+      display: flex;
+      font-size: 12px;
+      text-align: center;
+      
+      padding: 10px 0 30px;
+      border-bottom: 8px solid rgba(201, 199, 199, 0.4);
+     
+  }
+   .reommend-tiem{
+       flex: 1;
+   }
+
+  .reommend-tiem img {
+       width: 60px;
+      height: 60px;
+      top: 0;
+  }
+ 
+</style>
